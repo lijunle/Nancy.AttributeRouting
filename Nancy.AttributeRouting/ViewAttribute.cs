3e57f8de3a14f48bc3236bfae698cd0cff4036a8
Junle Li <lijunle@gmail.com>
2015-03-15 17:33:25 +0800

    Leverage view prefix attribute on declaring type.
    - Test case is passed.

--- Nancy.AttributeRouting/ViewAttribute.cs
+++ Nancy.AttributeRouting/ViewAttribute.cs
@@ -2,30 +2,48 @@
 {
     using System;
     using System.Diagnostics.CodeAnalysis;
+    using System.Reflection;
 
+    [AttributeUsage(AttributeTargets.Constructor | AttributeTargets.Method)]
     public class ViewAttribute : Attribute
     {
         private readonly string path;
 
         public ViewAttribute(string path)
         {
-            this.path = path;
+            this.path = path.TrimStart('/');
         }
 
-        public string Path
+        internal static string GetPath(MethodBase method)
         {
-            get { return this.path; }
+            var attr = method.GetCustomAttribute<ViewAttribute>();
+            if (attr == null)
+            {
+                return string.Empty;
+            }
+
+            string prefix = ViewPrefixAttribute.GetPrefix(method.DeclaringType);
+
+            return string.Format("/{0}/{1}", prefix, attr.path);
         }
     }
 
     [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Reviewed.")]
+    [AttributeUsage(AttributeTargets.Class)]
     public class ViewPrefixAttribute : Attribute
     {
         private readonly string prefix;
 
         public ViewPrefixAttribute(string prefix)
         {
-            this.prefix = prefix;
+            this.prefix = prefix.Trim('/');
+        }
+
+        internal static string GetPrefix(Type type)
+        {
+            var attr = type.GetCustomAttribute<ViewPrefixAttribute>();
+
+            return attr == null ? string.Empty : attr.prefix;
         }
     }
 }
