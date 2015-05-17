1ea91e81f48da50e4585e200d0ceb2af382e7b77
Junle Li <lijunle@gmail.com>
2015-05-17 21:42:14 +0800

    Write all public methods/properties documentation.

--- Nancy.AttributeRouting/RouteAttribute.cs
+++ Nancy.AttributeRouting/RouteAttribute.cs
@@ -6,6 +6,9 @@
     using System.Linq;
     using System.Reflection;
 
+    /// <summary>
+    /// The Route attribute indicates the routing path to handle the request.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Constructor | AttributeTargets.Method, AllowMultiple = true)]
     public abstract class RouteAttribute : Attribute
     {
@@ -89,12 +92,20 @@
         }
     }
 
+    /// <summary>
+    /// The RoutePrefix attribute. It decorates on class, indicates the path from route attribute on
+    /// the class and child class will be prefixed.
+    /// </summary>
     [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Reviewed.")]
     [AttributeUsage(AttributeTargets.Class)]
     public class RoutePrefixAttribute : Attribute
     {
         private readonly string prefix;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RoutePrefixAttribute"/> class.
+        /// </summary>
+        /// <param name="prefix">The string to prefix to the route attribute path.</param>
         public RoutePrefixAttribute(string prefix)
         {
             this.prefix = prefix.Trim('/');
@@ -118,54 +129,96 @@
         }
     }
 
+    /// <summary>
+    /// The Delete attribute. It indicates that this member hit with HTTP DELETE method.
+    /// </summary>
     [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Reviewed.")]
     public sealed class DeleteAttribute : RouteAttribute
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DeleteAttribute"/> class.
+        /// </summary>
+        /// <param name="path">The path to register into routing table.</param>
         public DeleteAttribute(string path)
             : base(path, HttpMethod.Delete)
         {
         }
     }
 
+    /// <summary>
+    /// The Get attribute. It indicates that this member hit with HTTP GET method.
+    /// </summary>
     [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Reviewed.")]
     public sealed class GetAttribute : RouteAttribute
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetAttribute"/> class.
+        /// </summary>
+        /// <param name="path">The path to register into routing table.</param>
         public GetAttribute(string path)
             : base(path, HttpMethod.Get)
         {
         }
     }
 
+    /// <summary>
+    /// The Options attribute. It indicates that this member hit with HTTP OPTIONS method.
+    /// </summary>
     [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Reviewed.")]
     public sealed class OptionsAttribute : RouteAttribute
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OptionsAttribute"/> class.
+        /// </summary>
+        /// <param name="path">The path to register into routing table.</param>
         public OptionsAttribute(string path)
             : base(path, HttpMethod.Options)
         {
         }
     }
 
+    /// <summary>
+    /// The Patch attribute. It indicates that this member hit with HTTP PATCH method.
+    /// </summary>
     [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Reviewed.")]
     public sealed class PatchAttribute : RouteAttribute
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PatchAttribute"/> class.
+        /// </summary>
+        /// <param name="path">The path to register into routing table.</param>
         public PatchAttribute(string path)
             : base(path, HttpMethod.Patch)
         {
         }
     }
 
+    /// <summary>
+    /// The Post attribute. It indicates that this member hit with HTTP POST method.
+    /// </summary>
     [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Reviewed.")]
     public sealed class PostAttribute : RouteAttribute
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PostAttribute"/> class.
+        /// </summary>
+        /// <param name="path">The path to register into routing table.</param>
         public PostAttribute(string path)
             : base(path, HttpMethod.Post)
         {
         }
     }
 
+    /// <summary>
+    /// The Put attribute. It indicates that this member hit with HTTP PUT method.
+    /// </summary>
     [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Reviewed.")]
     public sealed class PutAttribute : RouteAttribute
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PutAttribute"/> class.
+        /// </summary>
+        /// <param name="path">The path to register into routing table.</param>
         public PutAttribute(string path)
             : base(path, HttpMethod.Put)
         {
