792cd171e1552b1da32d22bc696c2085b12eba62
Junle Li <lijunle@gmail.com>
2015-06-06 17:19:11 +0800

    Use inheritdoc tag to avoid same or meaningless documentation.

--- Nancy.AttributeRouting/Registration.cs
+++ Nancy.AttributeRouting/Registration.cs
@@ -3,30 +3,22 @@
     using System.Collections.Generic;
     using Nancy.Bootstrapper;
 
-    /// <summary>
-    /// Register URL builder to Nancy IoC Container.
-    /// </summary>
+    /// <inheritdoc/>
     public class Registration : IRegistrations
     {
-        /// <summary>
-        /// Gets the collection registrations to register into Nancy IoC container.
-        /// </summary>
+        /// <inheritdoc/>
         public IEnumerable<CollectionTypeRegistration> CollectionTypeRegistrations
         {
             get { return null; }
         }
 
-        /// <summary>
-        /// Gets the instance registrations to register into Nancy IoC container.
-        /// </summary>
+        /// <inheritdoc/>
         public IEnumerable<InstanceRegistration> InstanceRegistrations
         {
             get { return null; }
         }
 
-        /// <summary>
-        /// Gets the types to register into Nancy IoC container.
-        /// </summary>
+        /// <inheritdoc/>
         public IEnumerable<TypeRegistration> TypeRegistrations
         {
             get { yield return new TypeRegistration(typeof(IUrlBuilder), typeof(UrlBuilder)); }
