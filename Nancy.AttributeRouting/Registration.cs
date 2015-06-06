3620299bfcfbb015427a9bb618358dc3482e62a4
Junle Li <lijunle@gmail.com>
2015-06-06 20:25:35 +0800

    Move the routing initialization work to Registration class static constructor.
    - So, the routing initialization work in done at the NancyBootstrapper.Initialize() time, which is a very beginning time.

--- Nancy.AttributeRouting/Registration.cs
+++ Nancy.AttributeRouting/Registration.cs
@@ -1,11 +1,28 @@
 ﻿namespace Nancy.AttributeRouting
 {
+    using System;
     using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
     using Nancy.Bootstrapper;
+    using Nancy.TinyIoc;
 
     /// <inheritdoc/>
     public class Registration : IRegistrations
     {
+        static Registration()
+        {
+            IEnumerable<MethodBase> methods = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(assembly => assembly.SafeGetTypes())
+                .Where(type => !type.IsAbstract && (type.IsPublic || type.IsNestedPublic))
+                .SelectMany(GetMembersWithRouteAttribute);
+
+            foreach (MethodBase method in methods)
+            {
+                RouteAttribute.AddToRoutings(AttributeRoutingResolver.Routings, method);
+            }
+        }
+
         /// <inheritdoc/>
         public IEnumerable<CollectionTypeRegistration> CollectionTypeRegistrations
         {
@@ -22,6 +39,20 @@
         public IEnumerable<TypeRegistration> TypeRegistrations
         {
             get { yield return new TypeRegistration(typeof(IUrlBuilder), typeof(UrlBuilder)); }
+        }
+
+        private static IEnumerable<MethodBase> GetMembersWithRouteAttribute(Type type)
+        {
+            IEnumerable<MethodBase> methods = type
+                .GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly)
+                .Where(HasRouteAttribute);
+
+            return methods;
+        }
+
+        private static bool HasRouteAttribute(MethodBase method)
+        {
+            return method.GetCustomAttributes<RouteAttribute>().Any();
         }
     }
 }
