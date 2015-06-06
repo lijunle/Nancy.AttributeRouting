c288cae28eaefb2f4f0f0b1281d62f724d16ac1e
Junle Li <lijunle@gmail.com>
2015-06-07 00:28:24 +0800

    Add InterfaceViewModel scaffolding.

---
+++ Nancy.AttributeRouting.Tests/ViewModels/InterfaceViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Nancy.AttributeRouting.Tests.ViewModels
+{
+    public interface IInterfaceViewModel
+    {
+        object Get();
+
+        object GetWithParamter(string value);
+    }
+
+    public class InterfaceViewModel : IInterfaceViewModel
+    {
+        public object Get()
+        {
+            return new { Result = "query-from-interface" };
+        }
+
+        public object GetWithParamter(string value)
+        {
+            return new { Result = value };
+        }
+    }
+}
