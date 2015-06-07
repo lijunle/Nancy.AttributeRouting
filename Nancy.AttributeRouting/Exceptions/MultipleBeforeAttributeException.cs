962900644f17eebd398650c3954eab6ad5155af5
Junle Li <lijunle@gmail.com>
2015-06-07 18:27:33 +0800

    Add MultipleBeforeAttributeException.

---
+++ Nancy.AttributeRouting/Exceptions/MultipleBeforeAttributeException.cs
@@ -0,0 +1,37 @@
+﻿namespace Nancy.AttributeRouting.Exceptions
+{
+    using System;
+    using System.Reflection;
+
+    [Serializable]
+    public class MultipleBeforeAttributeException : Exception
+    {
+        internal MultipleBeforeAttributeException(MethodBase method)
+            : base(GenerateMessage(method))
+        {
+        }
+
+        internal MultipleBeforeAttributeException(Type type)
+            : base(GenerateMessage(type))
+        {
+        }
+
+        private static string GenerateMessage(MethodBase method)
+        {
+            string message = string.Format(
+                "Multiple before attributes are decorated on method {0}.",
+                method.GetFullName());
+
+            return message;
+        }
+
+        private static string GenerateMessage(Type type)
+        {
+            string message = string.Format(
+                "Multiple before attributes are decorated on type {0}.",
+                type.FullName);
+
+            return message;
+        }
+    }
+}
