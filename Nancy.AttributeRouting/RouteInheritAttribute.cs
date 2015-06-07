be4e241a37719e361e27145d7b86895d778430e8
Junle Li <lijunle@gmail.com>
2015-06-07 15:53:34 +0800

    Add RouteInherit attribute.

---
+++ Nancy.AttributeRouting/RouteInheritAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace Nancy.AttributeRouting
+{
+    using System;
+
+    public class RouteInheritAttribute : Attribute
+    {
+        private readonly Type type;
+
+        public RouteInheritAttribute(Type type)
+        {
+            this.type = type;
+        }
+    }
+}
