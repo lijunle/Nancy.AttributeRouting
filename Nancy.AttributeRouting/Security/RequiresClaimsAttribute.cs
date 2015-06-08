1e81de67eba99db748c5fee82e9529dc334680ee
Junle Li <lijunle@gmail.com>
2015-06-09 03:18:05 +0800

    Add RequiresClaimsAttribute.

---
+++ Nancy.AttributeRouting/Security/RequiresClaimsAttribute.cs
@@ -0,0 +1,38 @@
+﻿namespace Nancy.AttributeRouting.Security
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Nancy.Security;
+    using Nancy.TinyIoc;
+
+    /// <summary>
+    /// The member decorated with <see cref="RequiresClaimsAttribute"/> indicates it requires
+    /// authentication and certain claims to be present.
+    /// </summary>
+    public class RequiresClaimsAttribute : BeforeAttribute
+    {
+        private readonly IEnumerable<string> claims;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RequiresClaimsAttribute"/> class.
+        /// </summary>
+        /// <param name="claims">The claims to be present for authentication.</param>
+        public RequiresClaimsAttribute(IEnumerable<string> claims)
+        {
+            this.claims = claims;
+        }
+
+        /// <inheritdoc/>
+        public RequiresClaimsAttribute(params string[] claims)
+            : this(claims.AsEnumerable())
+        {
+        }
+
+        /// <inheritdoc/>
+        public override Response Process(TinyIoCContainer container, NancyContext context)
+        {
+            return SecurityHooks.RequiresAuthentication().Invoke(context)
+                ?? SecurityHooks.RequiresClaims(this.claims).Invoke(context);
+        }
+    }
+}
