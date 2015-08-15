21b889e37863d276eef4e4cb164504be61961de8
Junle Li <lijunle@gmail.com>
2015-08-15 18:51:31 +0800

    Add home page and about page.

---
+++ Nancy.AttributeRouting.Examples/ViewModels/About.cs
@@ -0,0 +1,19 @@
+﻿namespace Nancy.AttributeRouting.Examples.ViewModels
+{
+    public class About
+    {
+        private readonly IUrlBuilder builder;
+
+        public About(IUrlBuilder builder)
+        {
+            this.builder = builder;
+        }
+
+        public string TodoUrl => "/todo";
+
+        public string HomeUrl => this.builder.GetUrl<Home>(m => m.Get());
+
+        [Get("about")]
+        public About Get() => this;
+    }
+}
---
+++ Nancy.AttributeRouting.Examples/ViewModels/Home.cs
@@ -0,0 +1,19 @@
+﻿namespace Nancy.AttributeRouting.Examples.ViewModels
+{
+    public class Home
+    {
+        private readonly IUrlBuilder builder;
+
+        public Home(IUrlBuilder builder)
+        {
+            this.builder = builder;
+        }
+
+        public string TodoUrl => "/todo";
+
+        public string AboutUrl => this.builder.GetUrl<About>(m => m.Get());
+
+        [Get("/")]
+        public Home Get() => this;
+    }
+}
