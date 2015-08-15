497444f436a07d4985688e24baeedc44ea770aed
Junle Li <lijunle@gmail.com>
2015-08-15 19:37:31 +0800

    Implement in memory DB schema.

---
+++ Nancy.AttributeRouting.Examples/Model/TodoDatabase.cs
@@ -0,0 +1,20 @@
+﻿namespace Nancy.AttributeRouting.Examples.Model
+{
+    using NMemory;
+    using NMemory.Tables;
+
+    public interface ITodoDatabase
+    {
+        ITable<TodoItem> Items { get; }
+    }
+
+    public class TodoDatabase : Database, ITodoDatabase
+    {
+        public TodoDatabase()
+        {
+            this.Items = this.Tables.Create(item => item.Id, new IdentitySpecification<TodoItem>(item => item.Id));
+        }
+
+        public ITable<TodoItem> Items { get; }
+    }
+}
