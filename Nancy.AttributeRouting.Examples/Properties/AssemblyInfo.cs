af3b14834952bcfca7a8805fd5cbb611a252edc7
Junle Li <lijunle@gmail.com>
2015-08-15 15:48:26 +0800

    Add examples project.

---
+++ Nancy.AttributeRouting.Examples/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Nancy.AttributeRouting.Examples")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Nancy.AttributeRouting.Examples")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("70c05534-6d54-45bd-b27a-eacc92f4799b")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
