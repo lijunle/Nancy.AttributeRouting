94d9b19049e89cdb0c6c21603e70d5dd083d2560
Junle Li <lijunle@gmail.com>
2015-08-21 00:08:25 +0800

    Enable code analysis and set all assembly version as 0.0.0.1 to resolve design warning.

--- Nancy.AttributeRouting.Tests/Properties/AssemblyInfo.cs
+++ Nancy.AttributeRouting.Tests/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("1af953f1-256b-492e-ac8c-3b6b379a8219")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
