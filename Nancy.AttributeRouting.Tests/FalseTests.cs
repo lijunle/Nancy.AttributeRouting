0f04720fd0d7b44ef1bef03686a3b6ed81aaf145
Junle Li <lijunle@gmail.com>
2015-08-22 01:17:13 +0800

    Add false test case to cover duplicated routing paths case.

---
+++ Nancy.AttributeRouting.Tests/FalseTests.cs
@@ -0,0 +1,36 @@
+﻿namespace Nancy.AttributeRouting.Tests
+{
+    using System;
+    using Exceptions;
+    using Testing;
+    using TinyIoc;
+    using ViewModels;
+    using Xunit;
+
+    public class FalseTests
+    {
+        [Fact]
+        public void Two_same_routing_paths_should_throw_exception()
+        {
+            Exception exception = null;
+
+            try
+            {
+                new Browser(
+                    new TestBootstrapper(DuplicatedRoutingPathsViewModel.TypeProvider));
+            }
+            catch (Exception e)
+            {
+                exception = e;
+            }
+            finally
+            {
+                Assert.NotNull(exception);
+                Assert.IsType<TinyIoCResolutionException>(exception);
+
+                Assert.NotNull(exception.InnerException);
+                Assert.IsType<DuplicatedRoutingPathsException>(exception.InnerException);
+            }
+        }
+    }
+}
